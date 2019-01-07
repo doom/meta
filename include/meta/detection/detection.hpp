6dfcab40847b9aa34ae7fa6d283e2047e7b3b9af
Doom <clement.doumergue@epitech.eu>
2019-01-07 15:39:06 -0500

    Add an implementation of the detection idiom

---
+++ include/meta/detection/detection.hpp
@@ -0,0 +1,41 @@
+/*
+** Created by doom on 07/01/19.
+*/
+
+#ifndef META_DETECTION_HPP
+#define META_DETECTION_HPP
+
+#include <type_traits>
+
+namespace meta
+{
+    template <typename, template <typename ...> typename MetaF, typename ...Args>
+    struct is_detected : std::false_type
+    {
+    };
+
+    template <template <typename ...> typename MetaF, typename ...Args>
+    struct is_detected<std::void_t<MetaF<Args...>>, MetaF, Args...> : std::true_type
+    {
+    };
+
+    template <template <typename ...> typename MetaF, typename ...Params>
+    inline constexpr bool is_detected_v = is_detected<void, MetaF, Params...>::value;
+
+    template <typename, typename Fallback, template <typename ...> typename MetaF, typename ...Args>
+    struct type_unless_detected
+    {
+        using type = Fallback;
+    };
+
+    template <typename Fallback, template <typename ...> typename MetaF, typename ...Args>
+    struct type_unless_detected<std::void_t<MetaF<Args...>>, Fallback, MetaF, Args...>
+    {
+        using type = MetaF<Args...>;
+    };
+
+    template <typename Fallback, template <typename ...> typename MetaF, typename ...Params>
+    using type_unless_detected_t = typename type_unless_detected<void, Fallback, MetaF, Params...>::type;
+}
+
+#endif /* !META_DETECTION_HPP */
---
+++ tests/tests-detection.cpp
@@ -0,0 +1,25 @@
+/*
+** Created by doom on 07/01/19.
+*/
+
+#include <meta/detection/detection.hpp>
+
+template <typename T>
+using member_type_test_detector = typename T::test;
+
+namespace
+{
+    struct with_test_member_type
+    {
+        using test = int;
+    };
+
+    struct without_test_member_type
+    {
+    };
+}
+
+static_assert(meta::is_detected_v<member_type_test_detector, with_test_member_type>);
+static_assert(!meta::is_detected_v<member_type_test_detector, without_test_member_type>);
+static_assert(std::is_same_v<meta::type_unless_detected_t<void, member_type_test_detector, with_test_member_type>, int>);
+static_assert(std::is_same_v<meta::type_unless_detected_t<void, member_type_test_detector, without_test_member_type>, void>);
