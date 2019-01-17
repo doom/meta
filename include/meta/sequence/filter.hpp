62b5f95de0beda457634df8299370fa543381a5c
Doom <clement.doumergue@epitech.eu>
2019-01-17 12:02:22 -0500

    Remove variadic packs in meta-predicates params, as they should be unary

--- include/meta/sequence/filter.hpp
+++ include/meta/sequence/filter.hpp
@@ -14,19 +14,25 @@ namespace meta
 {
     namespace details
     {
-        template <template <typename ...> typename Pred>
+        template <template <typename> typename Pred>
         struct list_or_empty
         {
-            template <typename ...Types>
-            using f = std::conditional_t<Pred<Types...>::value, list<Types...>, list<>>;
+            template <typename T>
+            using f = std::conditional_t<Pred<T>::value, list<T>, list<>>;
+        };
+
+        template <template <typename> typename MetaF, typename Sequence>
+        struct filter
+        {
+            using type = meta::flatten<meta::map<list_or_empty<MetaF>::template f, Sequence>>;
         };
     }
 
     /**
      * Filter the elements of a sequence based on a predicate
      */
-    template <template <typename ...> typename MetaF, typename Sequence>
-    using filter = flatten<map<details::list_or_empty<MetaF>::template f, Sequence>>;
+    template <template <typename> typename MetaF, typename Sequence>
+    using filter = typename details::filter<MetaF, Sequence>::type;
 }
 
 #endif /* !META_SEQUENCE_FILTER_HPP */
--- include/meta/sequence/map.hpp
+++ include/meta/sequence/map.hpp
@@ -11,10 +11,10 @@ namespace meta
 {
     namespace details
     {
-        template <template <typename ...> typename MetaF, typename Sequence>
+        template <template <typename> typename MetaF, typename Sequence>
         struct map;
 
-        template <template <typename ...> typename MetaF, typename ...Types>
+        template <template <typename> typename MetaF, typename ...Types>
         struct map<MetaF, list<Types...>>
         {
             using type = list<MetaF<Types>...>;
@@ -24,7 +24,7 @@ namespace meta
     /**
      * Apply a metafunction to each element of a sequence
      */
-    template <template <typename ...> typename MetaF, typename Sequence>
+    template <template <typename> typename MetaF, typename Sequence>
     using map = typename details::map<MetaF, Sequence>::type;
 }
 
