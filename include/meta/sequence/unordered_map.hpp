0e9cd040085b6d876e7acd38dda213778e2f887a
Doom <clement.doumergue@epitech.eu>
2019-01-17 13:27:39 -0500

    Add an assertion to ensure the types in a meta::unordered_map are pairs

--- include/meta/sequence/unordered_map.hpp
+++ include/meta/sequence/unordered_map.hpp
@@ -5,11 +5,27 @@
 #ifndef META_SEQUENCE_UNORDERED_MAP_HPP
 #define META_SEQUENCE_UNORDERED_MAP_HPP
 
+#include <meta/utils/pair.hpp>
+
 namespace meta
 {
+    namespace details
+    {
+        template <typename T>
+        struct is_pair : std::false_type
+        {
+        };
+
+        template <typename A, typename B>
+        struct is_pair<meta::pair<A, B>> : std::true_type
+        {
+        };
+    }
+
     template <typename ...Pairs>
     struct unordered_map
     {
+        static_assert((details::is_pair<Pairs>::value && ...), "meta::unordered_map: the elements should be pairs");
     };
 }
 
