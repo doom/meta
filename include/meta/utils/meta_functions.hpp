4c13fab1233e7cd4d5538b4e70f4b2b5f4d437f2
Doom <clement.doumergue@epitech.eu>
2019-01-21 00:19:57 -0500

    Add the meta::if_ and meta::if_c helpers

--- include/meta/utils/meta_functions.hpp
+++ include/meta/utils/meta_functions.hpp
@@ -12,6 +12,29 @@ namespace meta
 
     namespace details
     {
+        template <bool>
+        struct if_c_helper
+        {
+            template <typename T, typename F>
+            using type = T;
+        };
+
+        template <>
+        struct if_c_helper<false>
+        {
+            template <typename T, typename F>
+            using type = F;
+        };
+    }
+
+    template <bool b, typename T, typename F>
+    using if_c = typename details::if_c_helper<b>::template type<T, F>;
+
+    template <typename Bool, typename T, typename F>
+    using if_ = if_c<static_cast<bool>(Bool::value), T, F>;
+
+    namespace details
+    {
         template <template <typename> typename MetaF1, template <typename> typename MetaF2>
         struct compose
         {
--- tests/tests-meta_functions.cpp
+++ tests/tests-meta_functions.cpp
@@ -16,3 +16,8 @@ template <typename T>
 using div_by_four = typename meta::compose<div_by_two, div_by_two>::template func<T>;
 
 static_assert(div_by_four<meta::size_constant<4>>::value == 1);
+
+static_assert(std::is_same_v<meta::if_c<true, int, char>, int>);
+static_assert(std::is_same_v<meta::if_c<false, int, char>, char>);
+static_assert(std::is_same_v<meta::if_<meta::size_constant<2>, int, char>, int>);
+static_assert(std::is_same_v<meta::if_<meta::size_constant<0>, int, char>, char>);
